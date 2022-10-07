218b11a5ffcd103eb5a8d22cd7d7b79c18ec9762
oblisstified <k19079055@kcl.ac.uk>
2022-10-07 13:01:34 +0100

    first commit

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    
     frequencies = {}
+    for i in items:
+        if i not in frequencies:
+            frequencies.update({i : 1})
+        else:
+            frequencies[i] +=1
+            
+            
     # Your code goes here
     return frequencies
