1beafa48146aa35500fe6f0bc421f934e61a579f
oblisstified <k19079055@kcl.ac.uk>
2022-10-07 13:08:49 +0100

    added str

--- frequencies.py
+++ frequencies.py
@@ -8,8 +8,11 @@ def frequencies(items):
         if str(i) not in frequencies:
             frequencies.update({str(i) : 1})
         else:
-            frequencies[i] +=1
+            frequencies[str(i)] +=1
             
             
     # Your code goes here
+    print(frequencies)
     return frequencies
+
+frequencies([100, 'Hello', '100', '100', 100])
